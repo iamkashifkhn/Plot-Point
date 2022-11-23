75d28a62899b89fe7542d06d82408daf1eabc394
iamkashifusman <kashif.usman@assistechsol.com>
2022-11-23 19:00:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [points, setPoints] = useState([]);
+  const [popped, setPopped] = useState([]);
+
+  const handleClick = (e) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, { x: clientX, y: clientY }]);
+  };
+  const handleUndo = () => {
+    const newPoints = [...points];
+    const poppedPoint = newPoints.pop();
+    if(!poppedPoint) return;
+    setPopped([...popped, poppedPoint]);
+    setPoints(newPoints);
+  };
+
+  const handleRedo = () => {
+    const poppedPoint = popped.pop();
+    if(!poppedPoint) return;
+    setPoints([...points, poppedPoint]);
+    setPopped(popped);
+    
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={handleUndo}> Undo </button>
+      <button onClick={handleRedo}> Redo </button>
+      <div className="App" onClick={handleClick}>
+        {points.map((point, index) => {
+          return (
+            <div
+              key={index}
+              className="point"
+              style={{
+                left: point.x - 5 + "px",
+                top: point.y - 5 + "px",
+              }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
